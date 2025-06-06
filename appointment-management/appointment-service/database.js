3bbdb962a06728639c72df15a6aae265d64eb4b9
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2025-06-06 15:28:02 +0530

    Update database.js

--- appointment-management/appointment-service/database.js
+++ appointment-management/appointment-service/database.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.CHOREO_TESTDBCONN_DATABASENAME,
-  process.env.CHOREO_TESTDBCONN_USERNAME,
-  process.env.CHOREO_TESTDBCONN_PASSWORD,
+  process.env.CHOREO_TESTDBCON_DATABASENAME,
+  process.env.CHOREO_TESTDBCON_USERNAME,
+  process.env.CHOREO_TESTDBCON_PASSWORD,
   {
-    host: process.env.CHOREO_TESTDBCONN_HOSTNAME,
+    host: process.env.CHOREO_TESTDBCON_HOSTNAME,
     dialect: 'mysql',
-    port: process.env.CHOREO_TESTDBCONN_PORT,
+    port: process.env.CHOREO_TESTDBCON_PORT,
   }
 );
 
